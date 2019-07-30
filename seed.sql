9b28afad7093e9ce5da6197e9d29b7edbb5e508b
gitwenlei <ngwenlei@gmail.com>
2019-07-30 20:35:24 +0800

    Insert new data

--- seed.sql
+++ seed.sql
@@ -1,6 +1,10 @@
 INSERT INTO air_levels (sensor_level, status,description, recorded_at)
 VALUES(32, 'Fresh Air', 'The air is very clean!', CURRENT_TIMESTAMP);
 
+INSERT INTO air_levels (sensor_level, status,description, recorded_at)
+VALUES(421, 'Low Pollution', 'Very bad turn on air-purifier now!', CURRENT_TIMESTAMP);
+
+
 
 INSERT INTO room_states (state, activated_at, activation)
 VALUES('Air Con On', CURRENT_TIMESTAMP, true);
