95a911881776389add1f60ce353e8acdc0fd2f95
gitwenlei <ngwenlei@gmail.com>
2019-07-30 20:34:27 +0800

    Change file to MVC

--- index.js
+++ index.js
@@ -1,93 +1,69 @@
-
-console.log("starting up!!");
 const express = require('express');
 const methodOverride = require('method-override');
-const pg = require('pg');
-
-// Initialise postgres client
-const configs = {
-  user: 'fishie',
-  host: '127.0.0.1',
-  database: 'airdb',
-  port: 5432,
-};
-
+const cookieParser = require('cookie-parser');
 /**
  * ===================================
  * Configurations and set up
  * ===================================
  */
-
 // Init express app
 const app = express();
-
+// Set up middleware
+app.use(methodOverride('_method'));
+app.use(cookieParser());
+app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
-
-app.use(methodOverride('_method'));
-
 // Set react-views to be the default view engine
 const reactEngine = require('express-react-views').createEngine();
+
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', reactEngine);
 
-// Add CSS file in a public folder
-app.use(express.static(__dirname+'/public/'));
-
-const pool = new pg.Pool(configs);
-pool.on('error', function (err) {
-  console.log('idle client error', err.message, err.stack);
-});
-
-let now = new Date();
-console.log(now);
-console.log(now[0]);
-
-// =============================
-// links to pages
-// =============================
-const home = 'home.jsx';
 
 /**
  * ===================================
- * Routes
+ * ===================================
+ *                DB
+ * ===================================
  * ===================================
  */
 
-// GET Show All Sensor Readings
-app.get('/', (request, response) => {
-    const queryString = `SELECT * FROM air_levels`;
-    pool.query(queryString, (err, result) => {
-        if (err) {
-            console.error('query error:', err.stack);
-            response.send( 'query error' );
-        } else {
-            let data = {
-                levels: result.rows
-            };
-            response.render(home, data);
-        }
-    });
-});
+// db contains *ALL* of our models
+const allModels = require('./db');
 
+/**
+ * ===================================
+ * ===================================
+ * Routes
+ * ===================================
+ * ===================================
+ */
 
+// get the thing that contains all the routes
+const setRoutesFunction = require('./routes');
+
+// call it and pass in the "app" so that we can set routes on it (also models)
+setRoutesFunction(app, allModels);
 
 /**
  * ===================================
  * Listen to requests on port 3000
  * ===================================
  */
-const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => console.log('~~~ Tuning in to the waves of port '+PORT+' ~~~'));
 
 let onClose = function(){
-    console.log("closing");
-    server.close(() => {
-        console.log('Process terminated');
-        pool.end( () => console.log('Shut down db connection pool'));
-    })
+
+  server.close(() => {
+    console.log('Process terminated')
+    allModels.pool.end( () => console.log('Shut down db connection pool'));
+  })
 };
 
 process.on('SIGTERM', onClose);
