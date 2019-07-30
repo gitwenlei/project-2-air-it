22c0d46920584ff2c261b29500f0cbc132171a8a
gitwenlei <ngwenlei@gmail.com>
2019-07-30 20:37:21 +0800

    Added description field from data

--- views/home.jsx
+++ views/home.jsx
@@ -2,11 +2,12 @@ var React = require("react");
 
 class Home extends React.Component {
   render() {
-    console.log("date: ", this.props.levels[0].recorded_at.toString());
+    //console.log("date: ", this.props.levels[0].recorded_at.toString());
     const levelList = this.props.levels.map(level =>
         <div key={level.sensor_level} className="sensor_level">
             <p className="sensor_level">{level.sensor_level}</p>
             <p className="status">{level.status}</p>
+            <p className="status">{level.description}</p>
             <p className="date">{level.recorded_at.toString()}</p>
         </div>
         );
