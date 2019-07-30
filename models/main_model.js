ded0dbac19b6f7dfd935dd5cb8cfb75cbc48b923
gitwenlei <ngwenlei@gmail.com>
2019-07-30 20:39:51 +0800

    Added models folders for MVC

---
+++ models/main_model.js
@@ -0,0 +1,28 @@
+/**
+ * ===========================================
+ * Export model functions as a module
+ * ===========================================
+ */
+module.exports = (dbPoolInstance) => {
+
+    // `dbPoolInstance` is accessible within this function scope
+    let getAll = (callback) => {
+        const query = `SELECT * FROM air_levels`;
+        dbPoolInstance.query(query, (error, queryResult) => {
+            if (error) {
+                callback(error, null);
+            } else {
+                if (queryResult.rows.length > 0) {
+                    callback(null, queryResult.rows);
+                } else {
+                    callback(null, null);
+                }
+            }
+        });
+    };
+
+
+    return {
+        getAll,
+    };
+};
