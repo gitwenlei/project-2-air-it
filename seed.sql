bf0e1d26f972ee626db43efaaef6ff59a5639aa9
gitwenlei <ngwenlei@gmail.com>
2019-07-30 16:29:24 +0800

    Created seed sql file

---
+++ seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO air_levels (sensor_level, status,description, recorded_at)
+VALUES(32, 'Fresh Air', 'The air is very clean!', CURRENT_TIMESTAMP);
+
+
+INSERT INTO room_states (state, activated_at, activation)
+VALUES('Air Con On', CURRENT_TIMESTAMP, true);
