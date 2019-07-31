12a5e363534dc75f1b66c94956580b03b7423431
gitwenlei <ngwenlei@gmail.com>
2019-07-31 14:59:14 +0800

    Insert new data to room_states table

--- seed.sql
+++ seed.sql
@@ -2,10 +2,13 @@ INSERT INTO air_levels (sensor_level, status,description, recorded_at)
 VALUES(32, 'Fresh Air', 'The air is very clean!', CURRENT_TIMESTAMP);
 
 INSERT INTO air_levels (sensor_level, status,description, recorded_at)
-VALUES(421, 'Low Pollution', 'Very bad turn on air-purifier now!', CURRENT_TIMESTAMP);
+VALUES(421, 'Low Pollution', 'Close windows & turn on air-purifier now!', CURRENT_TIMESTAMP);
 
 INSERT INTO air_levels (sensor_level, status,description, recorded_at)
-VALUES(111, 'Normal Indoor Air', 'Turn on air-con', CURRENT_TIMESTAMP);
+VALUES(111, 'Normal Indoor Air', 'Turn on air-purifier', CURRENT_TIMESTAMP);
+
+INSERT INTO air_levels (sensor_level, status,description, recorded_at)
+VALUES(554, 'High Pollution - Action Recommended', 'Are the windows closed?', CURRENT_TIMESTAMP);
 
 INSERT INTO room_states (state, activated_at, activation)
 VALUES('Air Con On', CURRENT_TIMESTAMP, true);
