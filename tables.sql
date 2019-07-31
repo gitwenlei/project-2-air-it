b83a8a77d115e1da792a978d30f73b8a3d74b33e
gitwenlei <ngwenlei@gmail.com>
2019-07-31 16:35:21 +0800

    Change recorded_at & activated_at TIMESTAMP to TIMESTAMP DEFAULT now()

--- tables.sql
+++ tables.sql
@@ -3,13 +3,13 @@ CREATE TABLE IF NOT EXISTS air_levels (
   sensor_level INTEGER,
   status TEXT,
   description TEXT,
-  recorded_at TIMESTAMP
+  recorded_at TIMESTAMP DEFAULT now()
 );
 
 
 CREATE TABLE IF NOT EXISTS room_states (
   id SERIAL PRIMARY KEY,
   state TEXT,
-  activated_at TIMESTAMP,
-  activation BOOLEAN
+  activation BOOLEAN,
+  activated_at TIMESTAMP DEFAULT now()
 );
