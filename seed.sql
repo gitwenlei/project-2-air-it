ab5b888947f402d91c5d252dbf207949f8f3de9e
gitwenlei <ngwenlei@gmail.com>
2019-07-31 16:34:26 +0800

    Removed all activated_at & recorded_at data as it has been auto created in pqsl CREATE TABLE

--- seed.sql
+++ seed.sql
@@ -1,14 +1,14 @@
-INSERT INTO air_levels (sensor_level, status,description, recorded_at)
-VALUES(32, 'Fresh Air', 'The air is very clean!', CURRENT_TIMESTAMP);
+INSERT INTO air_levels (sensor_level, status, description)
+VALUES(32, 'Fresh Air', 'The air is very clean!');
 
-INSERT INTO air_levels (sensor_level, status,description, recorded_at)
-VALUES(421, 'Low Pollution', 'Close windows & turn on air-purifier now!', CURRENT_TIMESTAMP);
+INSERT INTO air_levels (sensor_level, status, description)
+VALUES(421, 'Low Pollution', 'Close windows & turn on air-purifier now!');
 
-INSERT INTO air_levels (sensor_level, status,description, recorded_at)
-VALUES(111, 'Normal Indoor Air', 'Turn on air-purifier', CURRENT_TIMESTAMP);
+INSERT INTO air_levels (sensor_level, status, description)
+VALUES(111, 'Normal Indoor Air', 'Make sure air-purifier & air-con is on');
 
-INSERT INTO air_levels (sensor_level, status,description, recorded_at)
-VALUES(554, 'High Pollution - Action Recommended', 'Are the windows closed?', CURRENT_TIMESTAMP);
+INSERT INTO air_levels (sensor_level, status, description)
+VALUES(554, 'High Pollution - Action Recommended', 'Are the windows closed?');
 
-INSERT INTO room_states (state, activated_at, activation)
-VALUES('Air Con On', CURRENT_TIMESTAMP, true);
+INSERT INTO room_states (state, activation)
+VALUES('Air Con On', true);
