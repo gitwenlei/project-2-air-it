e93337e7a48fdbb99a9c5f93f9f7a58518ebbbd3
gitwenlei <ngwenlei@gmail.com>
2019-07-31 23:34:39 +0800

    Added query for room_states

--- models/main_model.js
+++ models/main_model.js
@@ -8,13 +8,32 @@ module.exports = (dbPoolInstance) => {
     // `dbPoolInstance` is accessible within this function scope
     let getLatest = (callback) => {
         // select the latest entry
-        const query = `SELECT * FROM air_levels WHERE recorded_at= (SELECT MAX(recorded_at) FROM air_levels);`;
+        let data = {
+            air_levels: null,
+            room_states: null
+        };
+        let query = `SELECT * FROM air_levels WHERE recorded_at= (SELECT MAX(recorded_at) FROM air_levels);`;
         dbPoolInstance.query(query, (error, queryResult) => {
             if (error) {
                 callback(error, null);
             } else {
                 if (queryResult.rows.length > 0) {
-                    callback(null, queryResult.rows);
+                    // callback(null, queryResult.rows);
+                    data.air_levels = queryResult.rows;
+                    query = `SELECT * FROM room_states WHERE state='Air Con On' AND (activated_at=(SELECT MAX(activated_at) FROM room_states));`;
+                    dbPoolInstance.query(query, (error, queryResult) => {
+                        if (error) {
+                            callback(error, null);
+                        } else {
+                            if (queryResult.rows.length > 0) {
+                                data.room_states = queryResult.rows;
+                                callback(null, data);
+                            }
+                            // else {
+                            //     callback(null, null);
+                            // }
+                        }
+                    });
                 } else {
                     callback(null, null);
                 }
@@ -24,7 +43,7 @@ module.exports = (dbPoolInstance) => {
 
 
     let setRoomState = (content, callback) => {
-        console.log('content', content);
+        // console.log('content', content);
         const query = `INSERT INTO room_states (state, activation) VALUES($1, $2) RETURNING *`;
 
         let values = [content.state, content.activation];
