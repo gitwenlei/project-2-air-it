cf002537972c0313352a21cadde7a3fa0826bb27
gitwenlei <ngwenlei@gmail.com>
2019-07-31 14:57:20 +0800

    Added airCon state ctrller

--- controllers/main_ctrl.js
+++ controllers/main_ctrl.js
@@ -12,11 +12,9 @@ module.exports = (db) => {
             console.log(error)
         }else{
             if (result) {
-
-                data = {
+                var data = {
                     levels: result
                 }
-
                 response.render('index',data);
             }else{
                 console.log('done')
@@ -26,6 +24,21 @@ module.exports = (db) => {
   };
 
 
+  let airConOn = (request, response) => {
+    // console.log("body:", request.body);
+    db.main.setRoomState(request.body, (error,result)=>{
+        if(error){
+            console.log(error)
+        } else {
+            if (result) {
+                response.send('room state updated!')
+            }
+        }
+    })
+  };
+
+
+
   /**
    * ===========================================
    * Export controller functions as a module
@@ -33,5 +46,6 @@ module.exports = (db) => {
    */
   return {
     home: homePage,
+    intervene: airConOn
   };
 }
