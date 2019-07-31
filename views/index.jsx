36f6ac931951bcbec19dc8fc31a23d53d5b5ee78
gitwenlei <ngwenlei@gmail.com>
2019-07-31 23:38:48 +0800

    Removed map function for air_levels data & removed airconButton state check

--- views/index.jsx
+++ views/index.jsx
@@ -12,21 +12,37 @@ class Home extends React.Component {
     let date = now.getDate();
     let dateTime  = `${year}-${mth}-${date} ${hours}:${min}`;
 
-    const levelList = this.props.levels.map(level =>
-        <div key={level.sensor_level} className="sensor_reading">
-            <p className="recorded_at">Recorded On: {dateTime}</p>
-            <div className="sensor_level"><h1>{level.sensor_level}</h1></div>
-            <p className="status">{level.status}</p>
-            <p className="description">{level.description}</p>
-        </div>
-        );
+    const levels = this.props.levels[0];
+
+    // let airconButton = "";
+
+    // if (this.props.states[0].state === 'Air Con On' && this.props.states[0].activation != true) {
+    //     airconButton = <button type="button" id="air-con" className="room-btn btn btn-danger"></button>;
+    // } else {
+    //     airconButton = <button type="button" id="air-con" className="room-btn btn btn-primary"></button>;
+    // }
+    // return airconButton;
+
+    // check room_states table whether air-con is on
+    // let airconButton = "";
+    // if (aircon==="on") {
+    //     airconButton = <button type="button" id="air-con" className="room-btn btn btn-primary">AIR-CON On</button>
+    //     return airconButton
+    // }
+
+
     return (
       <Default title="air_levels">
         <div className="container">
             <div className="row">
                 <div className="sensor_wrapper col d-flex flex-column text-center">
                 <h1 className="title">Porpor's air</h1>
-                  {levelList}
+                <div key={levels.sensor_level} className="sensor_reading">
+                    <p className="recorded_at">Recorded On: {dateTime}</p>
+                    <div className="sensor_level"><h1>{levels.sensor_level}</h1></div>
+                    <p className="status">{levels.status}</p>
+                    <p className="description">{levels.description}</p>
+                </div>
                   <div className="btn-group" role="group" aria-label="Basic example">
                       <button type="button" id="refresh" className="room-btn btn btn-light">REFRESH</button>
                       <button type="button" id="air-con" className="room-btn btn btn-primary">AIR-CON OFF</button>
