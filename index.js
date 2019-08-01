bc45bc8e9f0e7b7f4afb8cde280c5f8a5d2ac654
gitwenlei <ngwenlei@gmail.com>
2019-08-01 14:22:38 +0800

    Added request module, added sensorCloud function to retrieve live sensor data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
-// const request = require('request');
+const request = require('request');
 
 
 /**
@@ -54,6 +54,89 @@ const setRoutesFunction = require('./routes');
 setRoutesFunction(app, allModels);
 
 
+//////////////////////////////////////////////
+// Retrieve live data of sensor from server //
+//////////////////////////////////////////////
+
+var gaUrl = 'https://us.wio.seeed.io/v1/node/GroveAirqualityA0/quality?access_token=fc5e2bde1dcc2360fc3b44be538dd639';
+
+let sensorCloud = () => {
+        request(gaUrl, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var data = JSON.parse(body); // convert incoming string to object
+                // console.log(data);
+                // console.log(typeof data)
+                var sensorValue = data.quality;
+                console.log('sensor:', sensorValue);
+
+                function quality(ppm) {
+                    if (ppm < 50) {
+                        return "Fresh Air";
+                    }
+                    if (ppm < 200) {
+                        return "Normal Indoor Air";
+                    }
+                    if (ppm < 400) {
+                        return "Low Pollution";
+                    }
+                    if (ppm < 600) {
+                        return "High Pollution - Action Recommended";
+                    }
+                    return "Very High Pollution - Take Action Immediately";
+                };
+
+                function action(ppm) {
+                    if (ppm < 50) {
+                        return "The air is awesome & clean!!";
+                    }
+                    if (ppm < 200) {
+                        return "Air is good but do start monitoring";
+                    }
+                    if (ppm < 400) {
+                        return "Ooooo... time to on air-purifier";
+                    }
+                    if (ppm < 600) {
+                        return "Close all windows immediately! On air-purifier & air-con";
+                    }
+                    return "This is serious! Wear a mask! All you sure windows are closed, and air-purifier and air-con are turned on? ";
+                };
+
+                // show status & description based on air quality
+                var status = quality(sensorValue);
+                var description = action(sensorValue);
+
+                console.log('status:', status);
+                console.log('description:', description);
+
+                var sensorObj = {
+                    sensor_level: sensorValue,
+                    status: status,
+                    description: description
+                };
+
+                var doThing = (error,result) => {
+                    console.log("kk")
+                    if(error){
+                        console.log(error)
+                    } else {
+                        console.log(result);
+                        // res.send(sensorObj);
+                    }
+                };
+
+                allModels.main.insertAir(sensorObj, doThing);
+            }
+        });
+};
+
+// ====================================
+// Only retrieve data at specific time
+// ====================================
+var someInterval = 60000;
+var reference = setInterval(sensorCloud, someInterval);
+
+//////////////////////////////////////////
+
 /**
  * ===================================
  * Listen to requests on port 3000
@@ -63,12 +146,16 @@ const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => console.log('~~~ Tuning in to the waves of port '+PORT+' ~~~'));
 
+
+
 let onClose = function(){
 
-  server.close(() => {
+    clearInterval(reference);
+
+    server.close(() => {
     console.log('Process terminated')
     allModels.pool.end( () => console.log('Shut down db connection pool'));
-  })
+    })
 };
 
 process.on('SIGTERM', onClose);
