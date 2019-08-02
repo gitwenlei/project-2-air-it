6be98ddf1b2d1c7f99c4af2c646f3d2037a8583f
gitwenlei <ngwenlei@gmail.com>
2019-08-03 00:30:26 +0800

    Created login form on landing page

--- views/index.jsx
+++ views/index.jsx
@@ -1,42 +1,28 @@
 var React = require('react');
 var Default = require('./layout/default');
 
-class Home extends React.Component {
+class Index extends React.Component {
   render() {
-
-    let now = this.props.levels[0].recorded_at;
-    let hours = now.getHours();
-    let min = now.getMinutes();
-    let year = now.getFullYear();
-    let mth = now.getMonth() + 1;
-    let date = now.getDate();
-    let dateTime  = `${year}-${mth}-${date} ${hours}:${min}`;
-
-    const levels = this.props.levels[0];
-
     return (
       <Default title="air_levels">
         <div className="container">
-            <div className="row">
-                <div className="sensor_wrapper col d-flex flex-column text-center">
-                <h1 className="title">Porpor's air</h1>
-                <div key={levels.sensor_level} className="sensor_reading">
-                    <p className="recorded_at">Recorded On: {dateTime}</p>
-                    <div className="sensor_level"><h1>{levels.sensor_level}</h1></div>
-                    <p className="status">{levels.status}</p>
-                    <p className="description">{levels.description}</p>
-                </div>
-                  <div className="btn-group">
-                      <button type="button" id="air-con" className="btn-air-con">AIR-CON OFF</button>
-                      <button type="button" id="air-pure" className="btn-air-pure">AIR PURIFIER OFF</button>
-                  </div>
+            <div className="col">
+                <div className="login_wrapper d-flex flex-column">
+                    <h1 className="title">Login</h1>
+
+                    <form action="/check" method="POST" >
+                        <p>user name</p>
+                            <input type="text" name="username" />
+                        <p>password</p>
+                            <input type="text" name="password" />
+                        <input type="submit" value="Submit" />
+                    </form>
                 </div>
             </div>
         </div>
-
       </Default>
     );
   }
 }
 
-module.exports = Home;
+module.exports = Index;
