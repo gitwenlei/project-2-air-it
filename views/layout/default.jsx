e9e6c801e6dd08c6761c3354d28e205496409b0d
gitwenlei <ngwenlei@gmail.com>
2019-08-02 10:40:31 +0800

    Added chart script

--- views/layout/default.jsx
+++ views/layout/default.jsx
@@ -7,6 +7,7 @@ class Default extends React.Component {
         <html>
             <head>
             <title>Air It</title>
+            <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"/>
             <link rel="stylesheet" type="text/css" href="/css/style.css"/>
             </head>
