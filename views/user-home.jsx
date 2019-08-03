1dc45fb7eb4a71a119b47e40999a33c0db28b3ab
gitwenlei <ngwenlei@gmail.com>
2019-08-04 01:57:56 +0800

    Created user home page template

---
+++ views/user-home.jsx
@@ -0,0 +1,42 @@
+var React = require('react');
+var Default = require('./layout/default');
+
+class UserPage extends React.Component {
+  render() {
+
+    let now = this.props.levels[0].recorded_at;
+    let hours = now.getHours();
+    let min = now.getMinutes();
+    let year = now.getFullYear();
+    let mth = now.getMonth() + 1;
+    let date = now.getDate();
+    let dateTime  = `${year}-${mth}-${date} ${hours}:${min}`;
+
+    const levels = this.props.levels[0];
+
+    return (
+      <Default title="air_levels">
+        <div className="container">
+            <div className="row">
+                <div className="sensor_wrapper col d-flex flex-column text-center">
+                <h1 className="title">Porpor's air</h1>
+                <div key={levels.sensor_level} className="sensor_reading">
+                    <p className="recorded_at">Recorded On: {dateTime}</p>
+                    <div className="sensor_level"><h1>{levels.sensor_level}</h1></div>
+                    <p className="status">{levels.status}</p>
+                    <p className="description">{levels.description}</p>
+                </div>
+                  <div className="btn-group">
+                      <button type="button" id="air-con" className="btn-air-con">AIR-CON OFF</button>
+                      <button type="button" id="air-pure" className="btn-air-pure">AIR PURIFIER OFF</button>
+                  </div>
+                </div>
+            </div>
+        </div>
+
+      </Default>
+    );
+  }
+}
+
+module.exports = UserPage;
