1bb0bd89d5eef8e3b00643b0b63703b638e63a9f
gitwenlei <ngwenlei@gmail.com>
2019-08-05 02:36:23 +0800

    Reset nav bar to bootstrap template default settings.

--- views/components/navbar.jsx
+++ views/components/navbar.jsx
@@ -4,15 +4,15 @@ class Navbar extends React.Component {
     render() {
     return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/home">Home</a>
+      <a className="navbar-brand" href="#">Home</a>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div className="navbar-nav">
-          <a className="nav-item nav-link active" href="/chart">24 Hr <span className="sr-only">(current)</span></a>
-          <a className="nav-item nav-link" href="#">1 Week</a>
-          <a className="nav-item nav-link" href="#">1 Month</a>
+          <a className="nav-item nav-link active" href="#">Hourly <span className="sr-only">(current)</span></a>
+          <a className="nav-item nav-link" href="#">Weekly</a>
+          <a className="nav-item nav-link" href="#">My Profile</a>
         </div>
       </div>
     </nav>
