33977d7c41c06755604f9ce125305a094f65fc05
gitwenlei <ngwenlei@gmail.com>
2019-08-05 02:37:13 +0800

    Remove nav bar template

--- views/layout/default.jsx
+++ views/layout/default.jsx
@@ -1,5 +1,5 @@
 var React = require('react');
-var Navbar = require('../components/navbar');
+// var Navbar = require('../components/navbar');
 
 class Default extends React.Component {
     render() {
@@ -12,7 +12,6 @@ class Default extends React.Component {
             <link rel="stylesheet" type="text/css" href="/css/style.css"/>
             </head>
             <body>
-            <Navbar title="nav-bar" />
             {this.props.children}
             <script src="/script.js"></script>
             <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
