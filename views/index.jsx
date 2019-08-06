deffba296296c6cbbba93714089ad8a188b9e816
gitwenlei <ngwenlei@gmail.com>
2019-08-06 08:19:21 +0800

    Added labels for login page

--- views/index.jsx
+++ views/index.jsx
@@ -8,14 +8,18 @@ class Index extends React.Component {
         <div className="container">
             <div className="col">
                 <div className="login_wrapper d-flex flex-column">
+                    <h1>Air It </h1>
+                    <img className="cover-img" src="./images/air_s.png" />
                     <h1 className="title">Login</h1>
-
                     <form action="/check" method="POST" >
                         <p>user name</p>
-                            <input type="text" name="username" />
+                        <input type="text" name="username" />
                         <p>password</p>
-                            <input type="text" name="password" />
+                        <input type="text" name="password" />
+                        <br />
+                        <br />
                         <input type="submit" value="Submit" />
+                        <p className="register-link">New User? Create a new account <a href="/register">here.</a></p>
                     </form>
                 </div>
             </div>
